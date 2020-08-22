0e8e917be9c6141d847e0d63fa48cee9bf0aface
joansaj <joannsaj@gmail.com>
2020-08-22 17:00:02 +0300

    edit and add documents

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
+function akanName () {
+  let monthOfBirth = document.getElementById("month").value;
+  let dateOfBirth = document.getElementById("date").value;
+  let yearOfBirth = document.getElementById("year").value;
+  let gender = document.getElementsByName("gender");
+}
 //Calculate day of the week
 let dayOfTheWeek = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
