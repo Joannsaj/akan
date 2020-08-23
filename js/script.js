de4e4159ab962c47f9c7957be96dee699f79cb51
joansaj <joannsaj@gmail.com>
2020-08-23 11:59:32 +0300

    add gender value

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@ function akanName () {
   let monthOfBirth = document.getElementById("month").value;
   let dateOfBirth = document.getElementById("date").value;
   let yearOfBirth = document.getElementById("year").value;
-  let gender = document.getElementsByName("gender");
+  let gender = document.getElementsByName("gender").value;
 }
+function getGender () {
+    if (gender.checked) {
+        return gender.value;
+    }else {
+        alert("Input gender")
+    }
+}
+ let genderValue = getGender();
+
 //Calculate day of the week
-let dayOfTheWeek = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
+/*let dayOfTheWeek = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7*/
